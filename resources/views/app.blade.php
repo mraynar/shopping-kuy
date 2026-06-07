8ff6d994e49fbfbcc279abd53c1082e9db947638
Muhammad Raynar Hammam <raynarham23@gmail.com>
2026-06-07 10:19:11 +0700

    fix: use config helper for midtrans snap script client key

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         <!-- Gunakan link sandbox jika masih tahap testing -->
-<script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('services.midtrans.client_key') }}"></script>
+<script src="{{ config('services.midtrans.snap_url') }}" data-client-key="{{ config('services.midtrans.client_key') }}"></script>
 
         <!-- Scripts -->
         @routes
