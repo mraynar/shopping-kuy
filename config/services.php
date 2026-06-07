036aa4bfa4aef077275d7c1a06ad7fb5ac4f7830
Muhammad Raynar Hammam <raynarham23@gmail.com>
2026-06-07 10:18:31 +0700

    fix: add midtrans service config so client_key is not null in blade

--- config/services.php
+++ config/services.php
@@ -41,5 +41,12 @@ return [
         'base_url' => 'https://rajaongkir.komerce.id/api/v1/',
     ],
 
+    'midtrans' => [
+        'server_key'    => env('MIDTRANS_SERVER_KEY'),
+        'client_key'    => env('MIDTRANS_CLIENT_KEY'),
+        'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+        'snap_url'      => env('MIDTRANS_SNAP_URL', 'https://app.sandbox.midtrans.com/snap/snap.js'),
+    ],
+
 ];
 
